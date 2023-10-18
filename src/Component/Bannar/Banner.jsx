398565109bb642106a685afe45714eb6e56aba9f
Sumiya Akhi <sumiya.akhi793@gmail.com>
2023-10-18 13:11:46 +0600

    Create Navber ,banner and footer

---
+++ src/Component/Bannar/Banner.jsx
@@ -0,0 +1,20 @@
+
+
+const Banner = () => {
+    return (
+        <div  >
+            <div className="hero min-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/MfGgKvN/banar3.webp)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5 text-4xl font-bold ">Find Your Dream Phone Here</p>
+      <input type="text" name="" placeholder="Find Your Phone" className="w-full p-2 h-10 rounded-full" id="" />
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,16 @@
+import Banner from "../Bannar/Banner";
+import Footer from "../SharedPage/Footer";
+import Navber from "../SharedPage/Navber";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navber></Navber>
+            <Banner></Banner>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
