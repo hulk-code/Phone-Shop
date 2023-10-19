08fb229bb82d59130b6ffeda6fbbc8bc677c98e7
Sumiya Akhi <sumiya.akhi793@gmail.com>
2023-10-19 21:12:45 +0600

    added another section(customer review)

---
+++ src/Component/Home/CustomerReview.jsx
@@ -0,0 +1,70 @@
+
+
+const CustomerReview = () => {
+    return (
+        <div>
+            <p className="text-5xl font-bold text-center mt-5 mb-10">Happy Customer Reviews</p>
+            <div className="grid grid-cols-1 lg:grid-cols-2 lg:w-3/4 mx-auto gap-10 mt-5  mb-5" >
+            <div className="card w-96 bg-base-100 ">
+            <div className="avatar mx-auto">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src="https://i.ibb.co/9pnG8VB/user.png" />
+  </div>
+</div>
+  <div className="card-body items-center text-center">
+    
+    <p>I recently purchased the XYZ Premium Headphones, and I must say, I'm thoroughly impressed! These headphones have exceeded my expectations in every way.
+</p>
+   <p className="text-bold text-center">Reviewed by: HappyCustomer123</p>
+    
+  </div>
+</div>
+            <div className="card w-96 bg-base-100 ">
+            <div className="avatar mx-auto">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src="https://i.ibb.co/9pnG8VB/user.png" />
+  </div>
+</div>
+  <div className="card-body items-center text-center">
+    
+    <p>I recently purchased the XYZ Premium Headphones, and I must say, I'm thoroughly impressed! These headphones have exceeded my expectations in every way.
+</p>
+   <p className="text-bold text-center">Reviewed by: HappyCustomer123</p>
+    
+  </div>
+</div>
+            <div className="card w-96 bg-base-100 ">
+            <div className="avatar mx-auto">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src="https://i.ibb.co/9pnG8VB/user.png" />
+  </div>
+</div>
+  <div className="card-body items-center text-center">
+    
+    <p>I recently purchased the XYZ Premium Headphones, and I must say, I'm thoroughly impressed! These headphones have exceeded my expectations in every way.
+</p>
+   <p className="text-bold text-center">Reviewed by: HappyCustomer123</p>
+    
+  </div>
+</div>
+            <div className="card w-96 bg-base-100 ">
+            <div className="avatar mx-auto">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src="https://i.ibb.co/9pnG8VB/user.png" />
+  </div>
+</div>
+  <div className="card-body items-center text-center">
+    
+    <p>I recently purchased the XYZ Premium Headphones, and I must say, I'm thoroughly impressed! These headphones have exceeded my expectations in every way.
+</p>
+   <p className="text-bold text-center">Reviewed by: HappyCustomer123</p>
+    
+  </div>
+</div>
+
+        </div>
+        </div>
+    );
+};
+
+export default CustomerReview;
