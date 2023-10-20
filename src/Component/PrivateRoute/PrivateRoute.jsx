d857204f57f1d0074cd2ebe18692c2e4f30ba8dd
Sumiya Akhi <sumiya.akhi793@gmail.com>
2023-10-20 09:51:16 +0600

    created privet route page

---
+++ src/Component/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { AuthContext } from "../FireBaseAuth/AuthProvider";
+import { Navigate } from "react-router-dom";
+
+
+// eslint-disable-next-line react/prop-types
+const PrivateRoute = ({children}) => {
+    const{user}=useContext(AuthContext)
+    if(user){
+        return children ;
+    }
+    return <Navigate to='/login'></Navigate>
+};
+
+export default PrivateRoute;
